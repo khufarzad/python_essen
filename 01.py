25ba70fd016a9e81b658cf862a98587887891ea6
khufarzad <160780389+khufarzad@users.noreply.github.com>
2024-03-14 17:55:01 +0330

    Session 01

---
+++ 01.py
@@ -0,0 +1,71 @@
+print(5 - 4)
+print(5 + 4)
+print(5 * 4)
+print(5 / 4)
+print(5 ** 4)
+print('Farzad Asgari')
+print("=============================")
+x = 3
+print(x)
+print(x*2)
+x = 2
+print(x)
+print(x+1)
+y = 4
+print(x + y)
+print("=============================")
+print(x, y, x+y, x-y, y-x)
+print('x:', x)
+print("=============================")
+result = 5 - 4
+print('result = 5 - 4 =', result)
+print("=============================")
+print('type(3):', type(3))
+print('type(3.14):', type(3.14))
+print('type("a"):', type('a'))
+# comment
+a = 8
+b = 7
+q = a / b
+q2 = a // b
+r = a % b
+
+print('q:', q)
+print('q2:', q2)
+print('r:', r)
+
+print("=============================")
+# int()
+# float()
+# str()
+t = 3
+z = float(t)
+s = str(t)
+print(t, type(t))  # int
+print(z, type(z))  # float
+print(s, type(s))  # str
+
+u = 3.14
+v = int(u)
+q = str(u)
+print('v:', v)
+print('q:', q, ', type(q):', type(q))
+print("=============================")
+x = 5
+x = x + 1
+print('new x:', x)
+print("=============================")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
