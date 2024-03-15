69109644997467f170afb9b53fa08742800f7487
Jahad-PC <jdsb.ac@gmail.com>
2024-03-14 23:14:36 -0700

    Session 02-01

---
+++ 02.py
@@ -0,0 +1,27 @@
+x = 8
+y = 5
+print('x:', x, ', and y:', y)
+print(f'x: {x}, and y: {y}')
+r = x - y
+print(
+f"""
+if x is equal to {x} and y is equal to {y},
+x - y = {x} - {y} = {r},
+x + y = {x} + {y} = {x+y},
+x * y = {x} * {y} = {x*y},
+x / y = {x} / {y} = {x/y},
+x ^ y = {x} ^ {y} = {x**y},
+x // y = {x} // {y} = {x//y},
+x % y = {x} % {y} = {x%y}
+"""
+)
+
+
+
+
+
+
+
+
+
+
